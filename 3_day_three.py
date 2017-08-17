7c2cf2fdef209350e270f6cce7c78555a4106a0d
jm.galeano1@gmail.com <jm.galeano1@gmail.com>
2017-08-17 14:52:45 -0400

    0_day_zero.py

---
+++ 3_day_three.py
@@ -0,0 +1,13 @@
+#Intro to Conditional Statements
+
+N = int(input().strip())
+
+if N % 2 ==1:
+	print("Weird")
+else:
+	if N % 2 == 0 and N > 20:
+		print("Not Weird")
+	elif N % 2 ==0 and 2 <= N <=5:
+		print ("Not Weird")
+	elif N % 2 ==0 and 6 <= N <= 20:
+		print ("Weird")
